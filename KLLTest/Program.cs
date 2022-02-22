ee4a1e42dacefbbe8e83b3edf49e85ee3c073d5b
TheLastRar <alastairbarnes123@hotmail.com>
2022-02-22 00:44:54 +0000

    Update Test project, cleaning it up a little bit

--- KLLTest/Program.cs
+++ KLLTest/Program.cs
@@ -11,24 +11,26 @@ namespace KLLTest
     internal class Program
     {
         [DllImport("user32.dll")]
-        static extern IntPtr GetKeyboardLayout(uint idThread);
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool GetKeyboardLayoutName([Out] StringBuilder pwszKLID);
 
         static void Main(string[] args)
         {
-            uint KeyboardLayout = (UInt32)GetKeyboardLayout(0).ToInt32();
-            KeyboardLayout = (KeyboardLayout >> 16) & 0xFFFF;
-            string id = KeyboardLayout.ToString("X8");
+            StringBuilder locale = new StringBuilder(new string(' ', 256));
+            GetKeyboardLayoutName(locale);
+            string id = locale.ToString();
 
-            RegistryKey layoutReg = Registry.LocalMachine.OpenSubKey("SYSTEM").OpenSubKey("CurrentControlSet").OpenSubKey("Control").OpenSubKey("Keyboard Layouts").OpenSubKey(id);
+            RegistryKey layoutReg = Registry.LocalMachine.OpenSubKey($@"SYSTEM\CurrentControlSet\Control\Keyboard Layouts\{id}");
+
 
             string dll = (string)layoutReg.GetValue("Layout File");
             string name = (string)layoutReg.GetValue("Layout Text");
 
             KLLNet.KLL kLL = new KLLNet.KLL();
-            //kLL.LoadDLL(@"C:\Windows\System32\" + "KBDGR.DLL");
-            //kLL.LoadDLL(@"C:\Windows\System32\" + "KBDSG.DLL");
-            kLL.LoadDLL(@"C:\Windows\System32\" + "KBDINTAM.DLL");
-            //kLL.LoadDLL(@"C:\Windows\System32\" + dll);
+            //kLL.LoadLayout(@"C:\Windows\System32\" + "KBDGR.DLL");
+            //kLL.LoadLayout(@"C:\Windows\System32\" + "KBDSG.DLL");
+            //kLL.LoadLayout(@"C:\Windows\System32\" + "KBDINTAM.DLL");
+            kLL.LoadLayout(@"C:\Windows\System32\" + dll);
 
         }
     }
